309fe1ae4c26a76615d00a6346b53e22adae8c3e
invy <1004018919@qq.com>
2019-01-14 23:27:03 +0800

    New unit test.

--- src/test/scala/SystolicArray/PEAWrapper.scala
+++ src/test/scala/SystolicArray/PEAWrapper.scala
@@ -19,6 +19,50 @@ class PEAWrapperTests(c: PEArrayWrapper) extends AdvTester(c) {
   reg_poke(c.io.fifoReset, 0)
   takestep()
   for(i <- 0 until 200) {
+    val testData = List.tabulate(18)(n => if(n % 3 == 1) 1 else 0)
+    val testWeight = List.fill(3)(1)
+    val expectedResult = List.tabulate(15)(
+      n => {
+        var sum = 0
+        for(i <- 0 until 3)
+          sum += testData(i + n) * testWeight(i)
+        sum
+      })
+    println("Data:" + testData)
+    println("Weight:" + testWeight)
+    println("Expected:" + expectedResult)
+    for(chan <- 0 until 3) {
+      dataIn(chan).inputs.enqueue(testData(0), testData(1), testData(2))
+      takesteps(3)()
+      weightIn(chan).inputs.enqueue(testWeight(0), testWeight(1), testWeight(2))
+      takesteps(3)()
+    }
+    takesteps(20)()
+    println("Clear")
+    for(chan <- 0 until 3) {
+      print(s"Result Channel $chan Get: ")
+      while(resultOut(chan).outputs.nonEmpty){
+        print(resultOut(chan).outputs.dequeue() + " ")
+      }
+      print("\n")
+    }
+    for(i <- 3 until 18) {
+      for(chan <- 0 until 3) {
+        dataIn(chan).inputs.enqueue(testData(i))
+        takesteps(3)()
+        weightIn(chan).inputs.enqueue(testWeight(i % 3))
+        takesteps(3)()
+      }
+    }
+    takesteps(20)()
+    for(chan <- 0 until 3) {
+      print(s"Result Channel $chan Get: ")
+      while(resultOut(chan).outputs.nonEmpty) {
+        print(resultOut(chan).outputs.dequeue() + " ")
+      }
+      print("\n")
+    }
+    /*
     val writeSequence = Random.shuffle(List.range(0, 6))
     for(chan <- writeSequence.indices) {
       val targetChan = writeSequence(chan) % 3
@@ -35,7 +79,7 @@ class PEAWrapperTests(c: PEArrayWrapper) extends AdvTester(c) {
           println("Result Channel " + chan + "  " + resultOut(chan).outputs.dequeue())
         }
       }
-    }
+      */
     /*
     for(chan <- dataIn.indices) {
       dataIn(chan).inputs.enqueue((i + chan) % 3 + 1)
@@ -56,7 +100,7 @@ class PEAWrapperTester extends ChiselFlatSpec {
   backends foreach { backend =>
     it should s"PEArrayWrapper $backend" in {
       Driver(
-        () => new PEArrayWrapper(3, 3, 32, 2, 5), backend
+        () => new PEArrayWrapper(1, 3, 32, 2, 5), backend
       )(c => new PEAWrapperTests(c)) should be (true)
     }
   }
